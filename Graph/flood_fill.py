d3cc39a2431a236c7168e7c57f2e3e5430b6ace0
Goun Na <gounna@gmail.com>
2024-07-16 04:31:00 +0900

    733. Flood Fill

--- Graph/flood_fill.py
+++ Graph/flood_fill.py
@@ -11,9 +11,9 @@ class Solution:
         A color of the pixel is image[sr][sc]
 
         Approach: 
-        recursively fill 
-        from the current position as a center to the left, right, top, down
-        For optimization, do not reapply the color if already has the target color
+        From the current position as a center 
+        To the left, right, top, down recursive change
+        Do not reapply the color if already has the target color
         
         Example:
         image = [[1,1,1],[1,1,0],[1,0,1]], 
@@ -28,19 +28,20 @@ class Solution:
         """
         len_row = len(image)
         len_col = len(image[0])
+        original_color = image[sr][sc]
 
-        def flood(row, col, prv_color):
+        def flood(row, col):
             if row > len_row - 1 or row < 0 or col > len_col - 1 or col < 0 or \
-                image[row][col] == color or image[row][col] != prv_color:
+                image[row][col] != original_color or image[row][col] == color:
                 return
             
             image[row][col] = color
 
-            flood(row, col-1, prv_color) # left
-            flood(row, col+1, prv_color) # right
-            flood(row-1, col, prv_color) # top
-            flood(row+1, col, prv_color) # down
-
-        flood(sr, sc, image[sr][sc])
+            flood(row, col-1) # left
+            flood(row, col+1) # right
+            flood(row-1, col) # top
+            flood(row+1, col) # down
+        
+        flood(sr, sc)
 
         return image
