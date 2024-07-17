1ec11c1c7265c51a76af634a35f216cc80dc158a
goungoun <gounna@gmail.com>
2024-07-18 07:03:06 +0900

    9. Palindrome Number

--- String/is_palindrome.py
+++ String/is_palindrome.py
@@ -4,17 +4,26 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         """
-        Check it is a palindrome or not
+        Check a given integer is a palindrome or not
         return True
+
+        Approach:
+        All negative integer is not palandrome
+        Return True if a reversed string and the string is the same
+        * reversed(s) will return iterable object, it should be converted to list and then back to string ex) "".join(list(reversed(s)))
+        * Or string can be revered using s[::-1]
 
         Example:
         x = 121
+        r = 121
         return True
 
         x = -121
+        r = 121-
         return False
 
         x = 10
+        r = 01
         return False
         """
 
@@ -22,6 +31,6 @@ class Solution:
             return False
 
         s = str(x)
-        rs = "".join(list(reversed(s)))
+        # rs = "".join(list(reversed(s)))
 
-        return s == rs
+        return s == s[::-1]
