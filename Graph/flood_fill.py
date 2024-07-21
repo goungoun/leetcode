4179b1c1b1d4922725ce512ac05ba8e93f07322e
goungoun <gounna@gmail.com>
2024-07-21 14:19:47 +0900

    733. Flood Fill

--- Graph/flood_fill.py
+++ Graph/flood_fill.py
@@ -4,44 +4,64 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         """
-        Change the color of a image by filling 4-direcional connected component
+        Change the pixel color of an image including 4-directional adjacents
         return image
 
-        sr, sc : starting point of the fill
-        A color of the pixel is image[sr][sc]
-
-        Approach: 
-        From the current position as a center 
-        To the left, right, top, down recursive change
-        Do not reapply the color if already has the target color
+        Example 1: original_color <> color
+        image = [[1,1,1],[1,1,0],[1,0,1]]
+        sr = 1, sc = 1
+        [1,1,1] 
+        [1,1,0] 
+        [1,0,1]
         
-        Example:
-        image = [[1,1,1],[1,1,0],[1,0,1]], 
-        (sr, sc) = (1, 1), color change : 1 -> 2
-
-        start: image[1][1]
+        color = 2
         [2,2,2] 
         [2,2,0] 
         [2,0,1]
 
-        return [[2,2,2], [2,2,0], [2,0,1]]
+        return [[2,2,2],[2,2,0],[2,0,1]]
+
+        Example 1: original_color == color
+        image = [[0,0,0],[0,0,0]]
+        sr = 0, sc = 0
+        [0,0,0]
+        [0,0,0]
+        color = 0
+
+        return [[0,0,0],[0,0,0]]
+
+        Approach:
+        First, change the color itself at the position (sr, sc)
+        Change it only when the current color is different from the color
+        Use BFS (or DFS) to fill other 4 adjacent colors: left, right, up and down
         """
-        len_row = len(image)
-        len_col = len(image[0])
-        original_color = image[sr][sc]
 
-        def flood(row, col):
-            if row > len_row - 1 or row < 0 or col > len_col - 1 or col < 0 or \
-                image[row][col] != original_color or image[row][col] == color:
-                return
+        m = len(image)
+        n = len(image[0])
+
+        # BFS
+        queue = deque([(sr,sc)])
+        target_color = image[sr][sc]
+
+        while queue:
+            (row, col) = queue.popleft()
+            curr_color = image[row][col]
             
-            image[row][col] = color
+            if curr_color == target_color and curr_color != color:
+                image[row][col] = color
 
-            flood(row, col-1) # left
-            flood(row, col+1) # right
-            flood(row-1, col) # up
-            flood(row+1, col) # down
-        
-        flood(sr, sc)
+                adjs = [(row,col-1), 
+                        (row,col+1), 
+                        (row-1,col), 
+                        (row+1,col)]
+
+                valid_adjs = [(r, c) for r, c in adjs \
+                    if r >= 0 and c >= 0 and r < m and c < n]
+
+                for row,col in valid_adjs:
+                    queue.append((row, col))
 
         return image
+
+        
+      
