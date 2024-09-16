673a2a34476c51c972631711380104e516482ae3
goungoun <gounna@gmail.com>
2024-09-16 09:10:31 +0900

    #. Merge Sort Example

--- Sort/merge_sort_example.py
+++ Sort/merge_sort_example.py
@@ -1,34 +1,42 @@
-def merge(left, right):
-  len_left = len(left)
-  len_right = len(right)
+def merge(arr1, arr2):
+	idx1 = 0
+	idx2 = 0
+	len_arr1 = len(arr1)
+	len_arr2 = len(arr2)
 
-  result = []
-  left_idx = 0
-  right_idx = 0
+	ret = []
 
-  while left_idx < len_left and right_idx < len_right:
-    if left[left_idx] <= right[right_idx]:
-      result.append(left[left_idx])
-      left_idx += 1
-    else:
-      result.append(right[right_idx])
-      right_idx += 1
+	while idx1 < len_arr1 and idx2 < len_arr2:
+		val1 = arr1[idx1]
+		val2 = arr2[idx2]
+    
+		if val1 >= val2:
+			ret.append(val1)
+			idx1 += 1
+		else:
+			ret.append(val2) 
+			idx2 += 1
 
-  if left_idx == len_left:
-    result.extend(right[right_idx:])
-  elif right_idx == len_right:
-    result.extend(left[left_idx:])
-
-  return result
-
+	if idx1 >= 0:
+		ret.extend(arr1[idx1:]) # Fix This
+	if idx2 >=0:
+		ret.extend(arr2[idx2:]) # Fix This
+	
+	return ret
+	
 def merge_sort(arr):
-  if len(arr) <= 1:
-    return arr
-  
-  mid_idx = len(arr)//2
-  left_arr = merge_sort(arr[:mid_idx])
-  right_arr = merge_sort(arr[mid_idx:])
+	"""
+	Divide and conquer resursive algorithm
+	"""
+	# base condition
+	if len(arr) == 1:
+		return arr
+	
+	mid = len(arr)//2
+	left = merge_sort(arr[mid:])
+	right = merge_sort(arr[:mid])
+	
+	return merge(left, right)
 
-  return merge(left_arr, right_arr)
-
-merge_sort([3,1,9,4,1])
+# Test
+merge_sort([3,5,1,2,9])
