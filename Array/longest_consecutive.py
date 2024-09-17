876ffd30a38af9390f12587ae1464fbe8d6359a3
goungoun <gounna@gmail.com>
2024-09-17 15:30:36 +0900

    128. Longest Consecutive Sequence (Medium)

--- Array/longest_consecutive.py
+++ Array/longest_consecutive.py
@@ -1,36 +1,36 @@
 # 128. Longest Consecutive Sequence
 # https://leetcode.com/problems/longest-consecutive-sequence/
 
-from collections import defaultdict
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         """
+        return the length of the consecutive elements sequence
+
         Example:
-        nums = [100, 4, 200, 1, 3, 2]
+        nums = [100, 4, 200, 1, 3, 2] * unsorted
+        The longest consecutive sequence is [1,2,3,4]
+        return 4
+
+        Approach:
+        Convert nums array list to a set
+        Narrow down candidates eleigible for a starter of its consecutive sequence
+        With that decreased scope, for each candidate, measure actual length of the sequence
         """
         if not nums or len(nums)==0:
             return 0
-        
+
         set_nums = set(nums)
-        max_cnt = 1
-        cnt = 1
-        n = 0
-
-        for num in nums:
-            if num - 1 not in set_nums:
-                cnt = 1
-                n = 1
-                while num + n in set_nums:
-                    cnt += 1
-                    n += 1
-                max_cnt = max(cnt, max_cnt)   
+        starters = set([num for num in nums if num-1 not in set_nums])
         
-        return max_cnt
+        max_length = 1
+        for starter in starters:
+            length = 1       
+            while starter + length in set_nums:
+                length += 1
+            max_length = max(length, max_length)
 
-# credit: niit
-# TODO: union and find
-
+        return max_length
+        
 
 
 
