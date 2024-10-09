96be5b24fdfb30b6ea0a75a7905923d1b13b66fe
goungoun <gounna@gmail.com>
2024-10-09 11:56:57 +0900

    119. Pascal's Triangle II

--- DynamicProgramming/pascal_triangle_II.py
+++ DynamicProgramming/pascal_triangle_II.py
@@ -5,11 +5,18 @@ from typing import List
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         """
-        Generate pascal's triangle with O(rowIndex) extra space
-        return p_row
+        Last row of pascal's triangle 
+        return row
+
+        * F/U: O(rowIndex) extra space
 
         Example:
         rowIndex = 3
+
+        1
+        1 1
+        1 2 1
+        1 3 3 1
         
         [1,1,1,1]
         [1,1,1,1]
@@ -20,18 +27,18 @@ class Solution:
 
         Approach: Backward iteration
         Initialize p row with all 1
-        Iterates backwards from i - 1 to 1 updating the value using previous row
-        In this way, we can updatge the current value having next column of the row untouched
+        Iterates backwards from i - 1 to 1 updating the value using the previous row
+        In this way, we can update the current value having the next column of the row untouched
 
-        Beats 85.95 %
+        Beats 89.25 %
         """
         if rowIndex is None:
             return []
 
-        p = [1] * (rowIndex + 1)
+        row = [1] * (rowIndex + 1)
         
         for i in range(2, rowIndex + 1):
             for j in range(i - 1, 0, -1):
-                p[j] = p[j] + p[j - 1]
+                row[j] = row[j] + row[j - 1]
         
-        return p
+        return row
