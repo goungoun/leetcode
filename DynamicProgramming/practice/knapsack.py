346942247d80840488b8a5de22131d352e9ba2bf
goungoun <gounna@gmail.com>
2024-10-09 22:49:20 +0900

    knapsack.py

---
+++ DynamicProgramming/practice/knapsack.py
@@ -0,0 +1,36 @@
+# Leet code for Knap Sack problem is not found. 
+# So, I wrote the python code for my practice after watching Tushar Roy's solution.
+# https://www.youtube.com/watch?v=8LusJS5-AGo&list=PLrmLmBdmIlpsHaNTPP_jHHDx_os9ItYXr&index=2
+
+n_items = 4
+values = [1,4,5,7]
+weights = [1,3,4,5]
+weight_limit = 7
+
+T = [[0] * (weight_limit+1) for _ in range(n_items)]
+
+for r in range(n_items):
+    for c in range(weight_limit + 1):
+        if r == 0:
+            T[0][c] = values[0] if c >= weights[0] else 0
+        else:
+            add = values[r] + T[r-1][weight_limit - weights[r]] if c >= weights[r] else 0 # Fix This!!
+            or_not = T[r-1][c]
+            T[r][c] = max(add, or_not)
+
+import numpy as np
+print(np.array(T))
+
+"""
+Wrong Result:
+[[ 0  1  1  1  1  1  1  1]
+ [ 0  1  1  5  5  5  5  5]
+ [ 0  1  1  5 10 10 10 10]
+ [ 0  1  1  5 10 10 10 10]]
+ 
+Expected Result:
+[[0 1 1 1 1 1 1 1]
+ [0 1 1 4 5 5 5 5]
+ [0 1 1 4 5 6 6 9]
+ [0 1 1 4 5 7 8 9]]
+"""
