1665d7671c5963c2fb96d0c01bdb93e1abbeef8f
goungoun <gounna@gmail.com>
2024-10-12 11:21:23 +0900

    #. Pascals triangle III

---
+++ DynamicProgramming/pascals_triangle_III.py
@@ -0,0 +1,25 @@
+"""
+In some cases, Top-down approach is better as it solves related problems only.
+For example, C(5,4) 
+"""
+
+~~~
+i) Top Down
+1
+1 1
+- 2 1
+- - 3 1
+- - - 4 1
+- - - - 5 -
+
+ 
+ii) bottom up
+1
+1 1
+1 2 1
+1 3 3 1 
+1 4 6 4 1
+1 5 10 10 5 -
+~~~
+
+# ref: Dynamic Programming by Meenakshi
