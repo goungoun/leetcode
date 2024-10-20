33a0b877345d06451143e13909ab3213b7094df0
goungoun <gounna@gmail.com>
2024-10-20 14:25:34 +0900

    67. Add Binary

--- Bits/add_binary.py
+++ Bits/add_binary.py
@@ -1,15 +1,13 @@
 # 67. Add Binary
-# https://leetcode.com/problems/add-binary/
+# https://leetcode.com/problems/add-binary
+
+from collections import deque 
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         """
         Add two binary numbers, a + b
         return sum
-
-        Idea:
-        Use Stack to access the number at the end of the string
-        (Or it would be good to use two indexes for each number)
 
         Example:
         a = "11", b = "1"
@@ -23,45 +21,59 @@ class Solution:
           +1011
           -----
           10101
+          
+        Approach: Use Stack
+        Use Stack to access the number at the end of the string
         """
+        if not a and not b:
+            return ""
 
-        l1 = [int(x) for x in a]
-        l2 = [int(x) for x in b]
+        l1 = [int(x) for x in a] if a else []
+        l2 = [int(x) for x in b] if b else []
 
         carry = 0
-        l = []
+        q = deque([])
 
         while l1 or l2:
             n1 = l1.pop() if l1 else 0
             n2 = l2.pop() if l2 else 0
 
             carry, r = divmod(n1 + n2 + carry,2)
-            l.append(str(r))
-
-        if carry > 0:
-            l.append(str(carry))
-
-        return "".join(l[::-1])
-
-    def addBinary_bak(self, a: str, b: str) -> str:
-        """
-        A queue for a result
-        q.appendleft(r), "".join(map(str, q))
-        """
-        s1 = [int(x) for x in a]
-        s2 = [int(x) for x in b]
-
-        carry = 0
-        q = collections.deque([])
-
-        while s1 or s2:
-            p1 = s1.pop() if s1 else 0
-            p2 = s2.pop() if s2 else 0
-
-            carry, r = divmod(p1 + p2 + carry,2)
             q.appendleft(r)
 
         if carry > 0:
             q.appendleft(carry)
 
-        return "".join(map(str, q))
+        return "".join(map(str,q))
+
+    def addBinary_2(self, a: str, b: str) -> str:
+        """
+        Approach: Index
+        Start from the end of each string
+        While decreasing indexs, add numbers and a carry
+        """
+        if not a and not b:
+            return ""
+
+        l1 = [int(x) for x in a] if a else []
+        l2 = [int(x) for x in b] if b else []
+
+        carry = 0
+        q = deque([])
+        i = len(l1)-1
+        j = len(l2)-1
+
+        while i >= 0 or j >= 0:
+            n1 = l1[i] if i >= 0 else 0
+            n2 = l2[j] if j >= 0 else 0
+
+            carry, r = divmod(n1 + n2 + carry, 2)
+            q.appendleft(r)
+
+            i -= 1
+            j -= 1
+
+        if carry > 0:
+            q.appendleft(carry)
+
+        return "".join(map(str,q))
