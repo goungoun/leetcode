16f43cb5744accfdd96668262e657ea5bb3bc5a6
Goun Na <gounna@gmail.com>
2024-10-23 22:56:13 +0900

    56. Merge Intervals (Medium)

--- Array/merge_interval.py
+++ Array/merge_interval.py
@@ -7,10 +7,18 @@ class Solution:
         Merge overlapping intervals
         return merged_intervals
 
+        Example 1: Overlapped
+        intervals = [[1,3],[2,6],[8,10],[15,18]]
+        return [[1,6],[8,10],[15,18]]
+
+        Example 2: Not overlapped (hidden)
+        intervals = [[1,4],[5,6]]
+        return [[1,4],[5,6]]
+
         Approach:
-        Sort the list based on the first element, start
-        If previous end is larger or equal than the current start, update the latest interval
-        If not overlapped, add the interval
+        Sort the list based on the first element, start.
+        If previous end is larger or equal than the current start, update the latest interval.
+        If not overlapped, add the interval to the result.
         """
 
         if not intervals:
