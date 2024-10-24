b42b8cffa92dec1bc5b57a6a46a9f48a44fad4b9
Goun Na <gounna@gmail.com>
2024-10-24 18:50:31 +0900

    1. Two Sum

--- Search/two_sum.py
+++ Search/two_sum.py
@@ -4,8 +4,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
+        Find two numbers in nums such that they add up to target.
+        return [i, j]
+
+        Example:
+        nums = [2,7,11,15], target = 9
+        return [0,1]
+        
         Approach:
-        Buliding dictionary and search number within the same for loop. T=O(n)
+        Buliding dictionary and search number within the same for loop. - well known approach!
+        T=O(n), S=O(n)
         """
         d = {} # key=num, value=index
 
@@ -18,16 +26,16 @@ class Solution:
 
     def twoSum_bak(self, nums: List[int], target: int) -> List[int]:
         """
-        Approach:
-        This is easier approach to come up with easily from the beginning 
-        You see, not a nested for loop it is also T = O(n) solution.
+        Approach: 
+        This is easier approach to come up with easily from the beginning.
+        Two phases, build dictionary and use. 
+        T=O(n), S=O(n) - acceptable!
         """
-        d = {} # key: nums, values: index
-        for i, num in enumerate(nums):
-            d[num] = i
+        d = {num:i for i, num in enumerate(nums)} # key: nums, values: index
 
         for i, num in enumerate(nums):
             search = target - num
             if search in d and i != d[search]:
                 return [d[search], i]
+
 
