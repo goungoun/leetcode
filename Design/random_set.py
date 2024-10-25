a9506d9f4fcc8005309dfd26e6aa6bf9f4d734df
Goun Na <gounna@gmail.com>
2024-10-26 00:37:44 +0900

    380. Insert Delete GetRandom O(1)

--- Design/random_set.py
+++ Design/random_set.py
@@ -28,6 +28,7 @@ class RandomizedSet:
     def insert(self, val: int) -> bool:
         """
         Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+        T=O(1)
         """
         if val not in self.data:
             self.dummy.append(val)
@@ -38,14 +39,15 @@ class RandomizedSet:
     def remove(self, val: int) -> bool:
         """
         Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+        T=O(1)
         """
         if val in self.data:
+            idx = self.data[val]
             last_idx = len(self.dummy)-1
             last_val = self.dummy[last_idx]
 
-            idx = self.data[val]
-            self.data[last_val] = idx
             self.dummy[idx] = last_val
+            self.data[last_val] = idx            
             
             del self.data[val]
             self.dummy.pop()
@@ -57,6 +59,7 @@ class RandomizedSet:
     def getRandom(self) -> int:
         """
         Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+        T=O(1)
         """
         if self.data:
             idx = random.randint(0, len(self.dummy)-1)
