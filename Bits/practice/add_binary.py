cabdc2927a46b2ef54287801435dd4b224697950
Goun Na <gounna@gmail.com>
2025-03-05 22:14:27 +0900

    67. Add Binary

--- Bits/practice/add_binary.py
+++ Bits/practice/add_binary.py
@@ -1,2 +1,35 @@
 # 67. Add Binary
 # https://leetcode.com/problems/add-binary
+
+from collections import deque 
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if not a and not b:
+            return ""
+        elif not a:
+            return b
+        elif not b:
+            return a
+        
+        i = len(a)-1
+        j = len(b)-1
+
+        carry = 0
+        q = deque([])
+ 
+        while i >= 0 or j >= 0:
+            n1 = int(a[i]) if i >= 0 else 0
+            n2 = int(b[j]) if j >= 0 else 0
+
+            ?, ? = divmod(n1 + n2 + carry, 2) # Complete here: which one is the remainder
+            q.?(r) # Complete here
+
+            i -= 1
+            j -= 1
+
+        if carry > 0:
+            q.?(carry) # Complete here
+
+        return "".join(map(str,q))
+
