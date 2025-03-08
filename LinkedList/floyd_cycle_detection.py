1055c6e09e906cc647507befc00f513a39212066
Goun Na <gounna@gmail.com>
2025-03-08 14:37:00 +0900

    141. Linked List Cycle

--- LinkedList/floyd_cycle_detection.py
+++ LinkedList/floyd_cycle_detection.py
@@ -12,19 +12,19 @@ class Solution:
         """
         Floyd's Cycle Detection Algorithm, also known as the "tortoise and hare"
 
-        Example 1:
+        Example 1: Cycle exists
         head = [3,2,0,-4], pos = 1
         +1: 3 2 0 "-4" 2 0 -4 2 ...
         +2: 3 0 2 "-4" 0 2 ...
         return True
 
-        Example 2:
+        Example 2: Cycle exists
         head = [1,2], pos = 0
         +1: 1 2 "1" 2 1 2 1 2 ...
         +2: 1 1 "1" 1 1 1 1 1 ...
         return True
 
-        Example 3:
+        Example 3: No cycle (pos= -1)
         head = [1], pos = -1
         +1: 1 <End>
         +2: 1 <End>
