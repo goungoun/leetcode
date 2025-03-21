865839fc72f60e641100e95b4eafae3a4af479f1
Goun Na <gounna@gmail.com>
2025-03-22 01:47:54 +0900

    278. First Bad Version

--- Search/first_bad_version.py
+++ Search/first_bad_version.py
@@ -10,10 +10,17 @@ class Solution:
         Looking for a way to figure out the first bad version minimizing the isBadVersion(n) API call
         return ver
 
-        Example: n = 5
+        Example: n = 5, bad = 4
         isBadVersion(5) => True
         isBadVersion(4) => True
         isBadVersion(3) => False
+
+        1 2 3 4 5 
+        F F F T T
+
+        mid = (1+5)//2 = 3 -> F (Go right)
+        mid = (4+5)//2 = 4 -> T (Go left)
+        mid = (4+4)//2 = 4 -> T
 
         return 4
 
@@ -27,7 +34,8 @@ class Solution:
         while l <= r:
             mid = (l + r)//2
             is_bad = isBadVersion(mid)
-
+            
+            # if bad, go to left to find the first one
             if is_bad:
                 ver = mid
                 r = mid - 1
