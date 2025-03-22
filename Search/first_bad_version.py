b04c5bf9729e78c4f89eef85dc8ec92d6bbb4d87
Goun Na <gounna@gmail.com>
2025-03-22 17:47:04 +0900

    278. First Bad Version

--- Search/first_bad_version.py
+++ Search/first_bad_version.py
@@ -7,8 +7,8 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         """
-        Looking for a way to figure out the first bad version minimizing the isBadVersion(n) API call
-        return ver
+        Looking for a fast way to detect the first bad version minimizing a number of API calls
+        return bad_version
 
         Example: n = 5, bad = 4
         isBadVersion(5) => True
@@ -17,19 +17,28 @@ class Solution:
 
         1 2 3 4 5 
         F F F T T
-
+        ----> <--
+        
         mid = (1+5)//2 = 3 -> F (Go right)
         mid = (4+5)//2 = 4 -> T (Go left)
         mid = (4+4)//2 = 4 -> T
 
         return 4
 
-        Approach:
+        Approach: Binary Search with two pointers
         Decreasing its number by one would result too many API calls
-        Apply binary search to decrease its search scope by half
+        Apply binary search to decrease its search space by half
+
+        T=O(log n), s=O(1)
         """
-        ver = n
-        l, r = 0, n
+        if n is None or n <= 0:
+            return -1
+            
+        # last version by default, but this is not the first version
+        bad_version = n
+
+        # search space: 1 ~ n
+        l, r = 1, n
 
         while l <= r:
             mid = (l + r)//2
@@ -37,9 +46,10 @@ class Solution:
             
             # if bad, go to left to find the first one
             if is_bad:
-                ver = mid
+                bad_version = mid
                 r = mid - 1
+            # if not, go to the right to find the bad one
             else:
                 l = mid + 1
         
-        return ver
+        return bad_version
