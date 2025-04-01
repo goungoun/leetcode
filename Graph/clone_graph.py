66faf2b127507fd77fc47832616a51b9e18aa9ef
Goun Na <gounna@gmail.com>
2025-04-02 05:11:27 +0900

    133. Clone Graph (Medium)

--- Graph/clone_graph.py
+++ Graph/clone_graph.py
@@ -14,27 +14,54 @@ from collections import deque
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         """
-        Clone a connected undirected graph
-        return the start node of a cloned graph
+        Clone a connected undirected (always bi-directional) graph
+        return new_start_node
 
-        Example:
-        Graph looks the same, but the nodes are all new with the same way of connections.
+        * adjacency list : a collection of unordered lists used to represent a finite graph (1-indexed)
 
-        Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
-        Output: [[2,4],[1,3],[2,4],[1,3]]
+        Example 1: 
+        adjList = [[2,4],[1,3],[2,4],[1,3]]
+        
+        1: {2,4}
+        2: {1,3}
+        3: {2,4}
+        4: {1,3}
+    
+        1 - 2
+        |   |
+        4 - 3
+        
+        return [[2,4],[1,3],[2,4],[1,3]]
 
-        Approach: 
-        Traverse the graph in BFS
+        Example 2:
+        adjList = [[2,3],[1,3],[1,2]]
+        
+        1: {2,4}
+        2: {1,3}
+        3: {1,2}
+    
+        1 - 2
+         \  |
+            3
+           
+        return  [[1,2],[2,3],[1,2]]
+    
+
+        Approach: BFS
         Prepare (create or get) a new node before the visit
         While visiting the existing node, get a new node and append the corresponding neighbors
+        Graph looks the same, but the nodes are all new with the same way of connections.
 
         Beats 90.46%
         """
         if not node:
-            return None
+            return
+            
+        # start node is given
+        # print (f"node={node.val}, neighbor = {[x.val for x in node.neighbors]}")
 
-        start_node = Node(node.val) # val=1
-        created = {node:start_node}
+        new_start_node = Node(node.val) # val=1
+        created = {node:new_start_node}
 
         q = deque([node])
 
@@ -52,5 +79,5 @@ class Solution:
             #print(f"old_node={old_node.val}:{[x.val for x in old_node.neighbors]}")  
             #print(f"new_node={new_node.val}:{[x.val for x in new_node.neighbors]}")    
 
-        return start_node
+        return new_start_node
     
