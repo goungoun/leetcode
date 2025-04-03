84fd048d6338b0c36e732e0964ebf867ff99a5f9
Goun Na <gounna@gmail.com>
2025-04-03 10:53:05 +0900

    1584. Min Cost to Connect All Points

--- Greedy/min_cost_connect.py
+++ Greedy/min_cost_connect.py
@@ -5,40 +5,43 @@ from heapq import heappop, heappush
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         """
-        Optimize cost connecting all points with the minimum distance
-        return min_cost
+        Connect all points with the minimum distance
+        return min_dist
 
-        * Manhattan distance (=cost) :|xi - xj| + |yi - yj|
+        * manhattan distance :|xi - xj| + |yi - yj|
 
-        Approach: Minimum Spanning Tree (Prim)
+        Approach: Minimum Spanning Tree
         Start with an any point and make a group
-        Expand the group by adding a nearest point, which is greedy
+        Expand the group by adding a nearest point which is greedy
         Repeat until the group includes all points
         """
+        if not points:
+            return 0
 
         n = len(points)
         min_cost = 0
-        min_heap = [(0,0)] # dist, idx of points
+        h = [(0,0)] # dist, idx of points
         group = set()
 
         # O(ElogV)
-        while len(group) < n:
-            # nearest 
-            dist, i = heappop(min_heap)
-
+        while len(group) < n: 
+            dist, i = heappop(h) # nearest from the group
             if i not in group:
                 group.add(i)
                 min_cost += dist
                 xi, yi = points[i]
 
-                # points order using heap
+                # points ordered by the distance
                 for j in range(n):
                     if j not in group:
                         xj, yj = points[j]
                         manhattan_dist = abs(xi-xj) + abs(yi-yj)
-                        heappush(min_heap, (manhattan_dist, j))
+                        heappush(h, (manhattan_dist, j))
 
         return min_cost
+        
+
+        
         
 # Reference: Gregg Hog
         
