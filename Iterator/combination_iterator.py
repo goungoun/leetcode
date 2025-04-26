1d8ee63048ebe9c367d6dd36cc60439e92474a02
Goun Na <gounna@gmail.com>
2025-04-26 18:43:08 +0900

    1286. Iterator for Combination (Medium)

--- Iterator/combination_iterator.py
+++ Iterator/combination_iterator.py
@@ -31,10 +31,5 @@ class CombinationIterator:
 
     def hasNext(self) -> bool:
         return self.nxt is not None
-        
 
-
-# Your CombinationIterator object will be instantiated and called as such:
-# obj = CombinationIterator(characters, combinationLength)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
+# See also: ../BackTracking/combination.py, ./combination_iterator_itertools.py
