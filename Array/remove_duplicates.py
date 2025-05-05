113350d86434a41946f231df38d8ca1b7c6ff833
Goun Na <gounna@gmail.com>
2025-05-05 15:35:49 +0900

    Remove Duplicates from Sorted Array II (Medium)

--- Array/remove_duplicates.py
+++ Array/remove_duplicates.py
@@ -19,29 +19,7 @@ class Solution:
         nums = [0,0,1,1,1,1,2,3,3]
         nums = [0,0,1,1,2,3,3,_,_]
         return 7
-
-        Approach: by niits
-        The point is how we can decide whether current number is valid or not.
-        Initialize k to 2 because the problem allows each unique element to appear at most twice.
-        Compare the current value with the element located at k - 2.        
-
-        T=O(n), S=O(1)
-        Beats 81.45%
-        """
-        if not nums:
-            return 0
-
-        k = 2
-
-        for i in range(2, len(nums)):
-            if nums[i] != nums[k - 2]:
-                nums[k] = nums[i]
-                k += 1 
-
-        return k
         
-    def removeDuplicates_my(self, nums: List[int]) -> int:
-        """
         Approach:
         Use cnt for each value
         Compare previous value and the current value for each iteration of the given array nums 
@@ -49,16 +27,15 @@ class Solution:
         If cnt is less than 2, in-place update to the num and increase k
 
         T=O(n), S=O(1)
-        Beats 53.91%
         """
-
         if not nums:
             return 0
 
+        n = len(nums)
         cnt =  1
         k = 1
 
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             if nums[i] == nums[i-1]:
                 cnt += 1
             else:
@@ -70,4 +47,4 @@ class Solution:
 
         return k
 
-# Referenced the solution by a leetcode user niits to compare my solution
+
