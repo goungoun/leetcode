a4f05e4d29c3c34560b77fa88f2e564166b92011
Goun Na <gounna@gmail.com>
2025-05-20 15:12:09 +0900

    21. Merge Two Sorted Lists

--- LinkedList/practice/merge_two_lists.py
+++ LinkedList/practice/merge_two_lists.py
@@ -7,6 +7,8 @@
 #         self.val = val
 #         self.next = next
 
+#### Warning!!! This is a buggy code for a practice
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if not list1:
@@ -15,27 +17,27 @@ class Solution:
         if not list2:
             return list1
 
+        # Fix This: we need more variable.. it will end up being empty
         dummy = ListNode()
-        curr = dummy
 
         while list1 and list2:
             if list1.val <= list2.val:
-                curr.next = list1            
+                dummy.next = list1            
                 list1 = list1.next
             else:
-                curr.next = list2
+                dummy.next = list2
                 list2 = list2.next
 
-            curr = curr.next
+            dummy = dummy.next
 
             #print(dummy)
 
         # Fix This: Use the test case to debug this code. list1 =[1,2,4], list2=[1,3,4]
         if not list1:
-            curr.next = list1
+            dummy.next = list1
 
         if not list2:
-            curr.next = list2
+            dummy.next = list2
 
         return dummy.next
 
