02d7cd2450563fd1b377d98d636ea5b7dea0971b
Goun Na <gounna@gmail.com>
2025-05-30 13:54:34 +0900

    210. Course Schedule II

---
+++ Graph/practice/course_schedule_II.py
@@ -0,0 +1,40 @@
+# 210. Course Schedule II
+# https://leetcode.com/problems/course-schedule-ii
+
+# Warning!!!!! It does not pass the test case. Need to deal with a cycle detection 
+# numCourses = 2, prerequisites = [[0,1],[1,0]]
+# output [1,0]
+# expected []
+
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        schedule = []
+        taken = set()
+
+        g = defaultdict(list)
+
+        # to take the course, must take other prep courses
+        for course, prep in prerequisites:
+            g[course].append(prep)
+
+        def can_finish(course):
+            if course in taken:
+                return
+
+            taken.add(course)
+
+            # take pre course
+            for prep in g[course]:
+                if prep not in taken:
+                    can_finish(prep)
+
+            # and then add the course to the schedule        
+            schedule.append(course)
+            return
+
+        for course in range(numCourses):
+            can_finish(course)
+
+        return schedule
