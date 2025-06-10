61ddf06e93844d29b118500be279c6acf384b2ae
Goun Na <gounna@gmail.com>
2025-06-10 10:01:49 +0900

    743. Network Delay Time (Medium)

---
+++ Graph/practice/network_depaly.py
@@ -0,0 +1,34 @@
+# 743. Network Delay Time (Medium)
+# https://leetcode.com/problems/network-delay-time
+
+# NEED TO FILL ???????
+
+from collections import defaultdict
+from heapq import heappush, heappop
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        if not times:
+            return 0
+
+        # Build a graph : departure -(interval)-> arrival
+        g = defaultdict(list)
+        for departure, arrival, interval in times:
+            g[departure].append((arrival, interval))
+
+        group = set()
+        h = [(0, k)] # (arrival_time, curr_node)
+        min_delay = 0
+
+        while h and len(group) < n:
+            arrival_time, node = heapq.heappop(h) # nearest neighbor
+
+            if node not in group:
+                group.add(node) 
+                min_delay = max(???????, min_delay)
+  
+                for neighbor_node, interval in g[node]:
+                    if neighbor_node not in group:
+                        heapq.heappush(h, (???????, neighbor_node))
+
+        return min_delay if len(group) == n else -1
