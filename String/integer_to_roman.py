6bebe0d4c207aa31dd81f8675a785a3a5198efe8
Goun Na <gounna@gmail.com>
2025-06-20 23:10:17 +0900

    12. Integer to Roman (Medium)

--- String/integer_to_roman.py
+++ String/integer_to_roman.py
@@ -1,4 +1,4 @@
-# 12. Integer to Roman
+# 12. Integer to Roman (Medium)
 # https://leetcode.com/problems/integer-to-roman
 
 class Solution:
@@ -25,21 +25,38 @@ class Solution:
         return "LVIII"
 
         Approach:
-        Create a dictionary to map a value to symbol
-        Sort the dictionary decreasing order
-        Decompose given num by divide the key to get mapped characters
+        First decompose numbers into each number and the weight
+        If the value is more than 5 and less than 9, decompose one more time
+        Find the right character from the value-symbol map and repeat
         """
-        d1 = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500: 'D', 1000:'M'}
-        d2 = {4:'IV', 9: 'IX', 40: 'XL',90: 'XC', 400: 'CD', 900: 'CM'}
+        if not num or not isinstance(num, int):
+            return ""       
+        
+        d = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500: 'D', 1000:'M',
+            4:'IV', 9: 'IX', 40: 'XL',90: 'XC', 400: 'CD', 900: 'CM'}
+        
+        decompose = [] 
+        weight = 1
 
-        d1.update(d2)
-        ret = []
+        while num > 0:
+            num, value = divmod(num, 10)
 
-        for k, v in sorted(d1.items(), reverse=True):
-            # s, r = divmod(num,k)
-            s = num // k
-            if s > 0:
-                ret.append(v*s)
-                num -= k*s
+            if value >= 5 and value < 9:
+                decompose.append((value-5, weight))
+                decompose.append((1, 5 * weight))
+            else:
+                decompose.append((value, weight))
+            weight *= 10
 
-        return "".join(ret)
+        n = len(decompose)
+        res = []
+
+        for i in range(n-1, -1, -1):
+            if decompose[i][0] in [4,9]:
+                symbol = d.get(decompose[i][1]*decompose[i][0])
+            else:
+                symbol = d.get(decompose[i][1])*decompose[i][0]
+
+            res.append(symbol)
+
+        return "".join(res)
