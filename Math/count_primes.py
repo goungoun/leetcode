5136e55a57040d9dbec3d03dbf9b72c9b29ad4a4
Goun Na <gounna@gmail.com>
2025-06-25 09:20:31 +0900

    204. Count Primes

---
+++ Math/count_primes.py
@@ -0,0 +1,34 @@
+# 204. Count Primes
+# https://leetcode.com/problems/count-primes/
+
+class Solution:
+    """
+    Get prime numbers less than n
+    return cnt_prime_numbers
+
+    * prime number: only divisible by one and itself
+
+    Approach: Sieve of Eratosthenes
+    List up all numbers less than n
+    Exclude all its multiples as they are divisible, non-prime integers
+    Remained numbers are prime numbers 
+    """
+    def countPrimes(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 0
+
+        # set all element to True, potentially they are prime number
+        is_primes = [True] * n
+
+        # except zero and one, prime number starts from 2
+        is_primes[0] = False
+        is_primes[1] = False
+       
+        for i in range(2, n): #for i in range(2, int(n ** 0.5) + 1):
+            # If i is prime number, let's exclude its multiples
+            if is_primes[i]:
+                for j in range(i + i, n, i): # multiples
+                    is_primes[j] = False
+
+        return sum(is_primes)
+      
