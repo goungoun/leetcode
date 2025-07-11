a8c2686d2ff6c4c4a01893550a542963526b28ab
Goun Na <gounna@gmail.com>
2025-07-11 20:24:28 +0900

    322. Coin Change (Medium)

---
+++ Greedy/coin_change_failed.py
@@ -0,0 +1,38 @@
+# 322. Coin Change (Medium)
+# https://leetcode.com/problems/coin-change
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        Minimum number of coins to make up the amount
+        return cnt_coins
+
+        Approach: Greedy
+        Works only when the the denominations can be multiplied like a Korean won
+        coins = [10, 50, 100, 500, 1000]
+        amount = 800
+        500 * 1 +  100 * 3 = 800
+        return 4
+        """
+        if not coins:
+            return 0
+
+        coins.sort(reverse=True)
+
+        cnt_coins = 0
+        
+        # T=O(n), S=O(1)
+        for coin in coins:
+            while coin <= amount and amount > 0:
+                amount -= coin
+                cnt_coins += 1
+
+        if amount > 0:
+            return -1 
+        
+        return cnt_coins
+
+# 51 / 189 testcases passed
+# Failed case : coins = [186,419,83,408]
+# To pass all test cases, use DP
+# See also: ../DynamicProgramming/coin_change.py
